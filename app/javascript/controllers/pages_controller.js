224cf1976caa79cd53d673dc153e8c36fec6cf1e
De Dannie <dedannie@Des-Air.local>
2021-07-22 11:37:25 +0700

    Add comments

--- app/javascript/controllers/pages_controller.js
+++ app/javascript/controllers/pages_controller.js
@@ -4,37 +4,42 @@ export default class extends Controller {
   static targets = [ "age_of_death_a", "year_of_death_a", "age_of_death_b", "year_of_death_b",
                     "answer1", "answer2", "answer3" ]
 
+  // Run everytime Submit button is clicked
   clicked() {
     const el_age_A = this.age_of_death_aTarget
     const el_year_A = this.year_of_death_aTarget
     const el_age_B = this.age_of_death_bTarget
     const el_year_B = this.year_of_death_bTarget
 
+    const el_answer1 = this.answer1Target
+    const el_answer2 = this.answer2Target
+    const el_answer3 = this.answer3Target
+
     const age_A = el_age_A.value
     const year_A = el_year_A.value
     const age_B = el_age_B.value
     const year_B = el_year_B.value
 
-    const el_answer1 = this.answer1Target
-    const el_answer2 = this.answer2Target
-    const el_answer3 = this.answer3Target
-
+    // Process for div#answer1
     this.people("A", age_A, year_A)
     let killed_by_A = this.killed    
     let answer_for_1 = this.answer
     el_answer1.innerHTML = answer_for_1
 
+    // Process for div#answer2
     this.people("B", age_B, year_B)
     let killed_by_B = this.killed   
     let answer_for_2 = this.answer
     el_answer2.innerHTML = answer_for_2
     
+    // Process for div#answer3
     let persons = [killed_by_A, killed_by_B]
     this.average(persons)
     let answer_for_3 = this.answer
     el_answer3.innerHTML = answer_for_3
   }
 
+  // Function for creating text for div#answer1 and div#answer2 by throwing letter, age and year value
   people(letter, age, year) {
     if (age < 0) { age = -1 }
     if (year < 0) { year = -1 }
@@ -46,6 +51,7 @@ export default class extends Controller {
     ", number of people killed on year " + this.substration + " is " + this.killed + "."
   }
 
+  // Creating Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1 and sum them
   fibonacci(age, year) {
     var numMax = year - age, x
     if (numMax > 0) {
@@ -57,6 +63,7 @@ export default class extends Controller {
     return sum || 0
   }
 
+  // Calculate average from arrays value and create text for div#answer3
   average(numbers) {
     var total = 0
     for(var i = 0; i < numbers.length; i++) {
@@ -68,6 +75,7 @@ export default class extends Controller {
     this.answer = 'So the average is (' + this.number_list + ')/' + this.length + ' = ' + this.average
   }
 
+  // Create list number backward
   list_of_numbers(numbers) {
     for(var list_number = "", addition = "", i = numbers.length - 1; i >= 0 ; i-- ){
       addition = i > 0 ? " + " : ""
--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class PagesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the root" do
     visit root_path
 
     fill_in "age_of_death_a", with: 10
@@ -12,5 +12,7 @@ class PagesTest < ApplicationSystemTestCase
     click_on "Submit"
   
     assert_selector "div#answer1", text: "Person A born on Year = 12 - 10 = 2, number of people killed on year 2 is 2."
+    assert_selector "div#answer2", text: "Person B born on Year = 17 - 13 = 4, number of people killed on year 4 is 7."
+    assert_selector "div#answer3", text: "So the average is (7 + 2)/2 = 4.5"
   end
 end
